fc03cf6b871021382268f706f072c51678aedafc
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2024-02-15 08:32:53 +0530

    script added

---
+++ contscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker stop pipecont
+docker rm pipecont
+docker build -t pipeimg .
+docker run -i --name pipecont --network=gomatha --ip 192.168.1.50  -p 6565:6565  pipeimg /bin/bash
+docker run -it --name pipecont pipeimg /bin/bash
+docker start pipecont
+docker exec pipecont ./startup.sh
