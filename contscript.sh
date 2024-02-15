b91f6d69681d93b3fbc84da25ab9cf3588f36e53
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2024-02-15 10:12:42 +0530

    Update contscript.sh

--- contscript.sh
+++ contscript.sh
@@ -3,6 +3,6 @@ docker stop pipecont
 docker rm pipecont
 docker build -t pipeimg .
 docker run -i --name pipecont --network=gomatha --ip 192.168.1.50  -p 6565:6565  pipeimg /bin/bash
-docker run -it --name pipecont pipeimg /bin/bash
+
 docker start pipecont
 docker exec pipecont ./startup.sh
