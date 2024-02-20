8d1b26b64cc1a90e4b045e0599d4c50680fb6872
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2024-02-20 13:18:46 +0530

    Delete contscript.sh

--- contscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker stop pipecont
-docker rm pipecont
-docker build -t pipeimg .
-docker run -i --name pipecont --network=gomatha --ip 192.168.1.50  -p 6565:6565  pipeimg /bin/bash
-
-docker start pipecont
-docker exec pipecont ./startup.sh
